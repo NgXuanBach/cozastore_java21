4b78ddee1515e9b9d0d967354f1f63951c726876
Nguyễn Xuân Bách <100914514+NgXuanBach@users.noreply.github.com>
2023-07-07 08:18:37 +0700

    add logger

--- src/main/java/com/cybersoft/cozastore_java21/controller/ProductController.java
+++ src/main/java/com/cybersoft/cozastore_java21/controller/ProductController.java
@@ -2,6 +2,9 @@ package com.cybersoft.cozastore_java21.controller;
 
 import com.cybersoft.cozastore_java21.payload.response.BaseResponse;
 import com.cybersoft.cozastore_java21.service.imp.ProductServiceImp;
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
     @Autowired
     private ProductServiceImp productServiceImp;
+    private Gson gson = new Gson();
+    private Logger logger = LoggerFactory.getLogger(ProductController.class);
 
     @GetMapping("/category/{id}")
     public ResponseEntity<?> getProductByCategory(@PathVariable int id) {
+        logger.info("Tham số " + id);
         BaseResponse response = new BaseResponse();
         response.setStatusCode(200);
         response.setData(productServiceImp.getProductByCategory(id));
+        logger.info(gson.toJson(response));
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
